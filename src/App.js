78bce98fd4cd18a7a9ecb2a94c740b8f571e2367
cabonilla <christian.bonilla01@gmail.com>
2021-06-01 19:45:00 -0700

    Fetching data from Firebase API using action creators.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { fetchCartData, sendCartData } from './store/cart-actions';
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch()
   const showCart = useSelector((state) => state.ui.cartIsVisible)
-  const cart = useSelector(state => state.cart)
-  const notification = useSelector(state => state.ui.notification)
+  const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
+
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [dispatch])
 
   useEffect(() => {
     if (isInitial) {
@@ -20,7 +24,9 @@ function App() {
       return;
     }
 
-    dispatch(sendCartData(cart))
+    if (cart.changed) {
+      dispatch(sendCartData(cart))
+    }
   }, [cart, dispatch])
 
   return (
